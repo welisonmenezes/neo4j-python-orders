57b9f46797b86d10b5ac36213eb502161e4e6776
welisonmenezes <welisonmenezes@hotmail.com>
2020-06-04 23:44:10 -0300

    Build the UI interface and add it into the Flask UI folder

---
+++ application/Views/UI/precache-manifest.b64d561b80f01daceea900e27c88cf2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25176ad55fcd32b6a01ed54dd2d9bc03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5a3486967e51564c928",
+    "url": "/static/css/2.b211811c.chunk.css"
+  },
+  {
+    "revision": "92770b880771fd49e948",
+    "url": "/static/css/main.de4b9a8b.chunk.css"
+  },
+  {
+    "revision": "f5a3486967e51564c928",
+    "url": "/static/js/2.cbc6e26e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cbc6e26e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92770b880771fd49e948",
+    "url": "/static/js/main.38fd326d.chunk.js"
+  },
+  {
+    "revision": "222e6851b2ddb632083c",
+    "url": "/static/js/runtime-main.79dff054.js"
+  }
+]);
